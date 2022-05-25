8f31924d5165cba213e1f31a7a320bb88fffbff5
Juyie <ghkdwndl1004@ewhain.net>
2022-05-25 17:48:02 +0900

    feat: add loading screen

---
+++ app/src/main/java/com/Phoenix/puding/IntroActivity.java
@@ -0,0 +1,28 @@
+package com.Phoenix.puding;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro); //xml , java 소스 연결
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent); //인트로 실행 후 바로 MainActivity로 넘어감.
+                finish();
+            }
+        }, 3000); //3초 후 인트로 실행
+    }
+
+    @Override protected void onPause () {
+        super.onPause();
+        finish();
+    }
+}
