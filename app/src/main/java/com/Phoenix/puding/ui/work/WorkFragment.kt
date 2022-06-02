7ffe5a79fe5d255cb0a290011c3c3b495fc7f7b5
Juyie <ghkdwndl1004@ewhain.net>
2022-06-02 15:10:22 +0900

    feat: add close tab

--- app/src/main/java/com/Phoenix/puding/ui/work/WorkFragment.kt
+++ app/src/main/java/com/Phoenix/puding/ui/work/WorkFragment.kt
@@ -5,18 +5,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.Phoenix.puding.MainActivity
 import com.Phoenix.puding.R
 import com.Phoenix.puding.SetGoalsActivity
 import com.Phoenix.puding.databinding.FragmentWorkBinding
+import com.Phoenix.puding.ui.home.HomeFragment
 
 
 class WorkFragment : Fragment()  {
 
     private var _binding: FragmentWorkBinding? = null
     lateinit var add_goal : ImageButton
+    lateinit var closeTabButton: ImageView
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -33,7 +38,7 @@ class WorkFragment : Fragment()  {
         _binding = FragmentWorkBinding.inflate(inflater, container, false)
         val root: View = binding.root
         add_goal = root.findViewById(R.id.add_note)
-
+        closeTabButton = root.findViewById(R.id.down)
 //        val textView: TextView = binding.textWork
 //        workViewModel.text.observe(viewLifecycleOwner) {
 //            textView.text = it
@@ -45,6 +50,10 @@ class WorkFragment : Fragment()  {
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
             startActivity(intent)
         })
+
+        closeTabButton.setOnClickListener {
+            (activity as MainActivity).replaceFragment(HomeFragment())
+        }
 
         return root
     }
